08a961f513760aa018c7041501e23230ceacda02
youssef20031 <43968405+youssef20031@users.noreply.github.com>
2019-01-09 20:31:04 +0200

    Add files via upload

---
+++ py19.py
@@ -0,0 +1,36 @@
+name1 = input("what's your name" ":")
+print("hi", name1)
+name2 = input("how are you"  ":")
+if name2 == "good":
+    print("nice")
+elif name2 == "bad":
+    print("i'am sorry")
+elif name2 == "not good":
+    print("it is okay")
+elif name2 == "nice":
+    print("very good")
+elif name2 == "fine":
+    print("nice")
+else:
+    print("hmmm, ok")
+name3 = input("what are your hobies?"  ":")
+if name3 == "coding":
+    print("good")
+elif name3 == "sports":
+    print("nice")
+elif name3 == "nothing":
+    print("do something")
+elif name3 == "shouting":
+    print("be calm")
+else:
+    print("ok")
+import time
+while True:
+    time.sleep(2)
+    print("thank you for using this python script")
+    break
+import time
+while True:
+    time.sleep(2)
+    print("created by youssef maged")
+    break
